b2d4ead66f938418fd68513ec8a4b494d37236a6
Shreevasa21 <141390315+Shreevasa21@users.noreply.github.com>
2023-10-06 08:19:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Sample JavaScript for handling booking and scheduling
+document.addEventListener('DOMContentLoaded', function() {
+    const bookingForm = document.getElementById('book-now');
+    const bookingButton = bookingForm.querySelector('.cta-button');
+
+    bookingButton.addEventListener('click', function() {
+        // Implement your booking and scheduling logic here
+        alert('Booking functionality will be implemented soon.');
+    });
+});
